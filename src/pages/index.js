49624b777965516a3d5bbfcdc295e3ade7422ba5
Dalibor Janeček <dalibor.janecek@gmail.com>
2024-10-04 13:27:17 +0200

    načítání počasí z OpenWeather

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+import fetchWeather from "@/utils/fetchWeather";
+
 export default function Home() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const weather = await fetchWeather("Olomouc");
+      setWeather(weather)
+    };
+    fetchData();
+    console.log(weather)
+  }, [])
+  
   return (
     <>
       <h1>Počasí</h1>
@@ -9,9 +23,9 @@ export default function Home() {
         </form>
       </div>
       <div className="weather-display">
-        <h2>Předpověď pro město</h2>
-        <p>Teplota: </p>
-        <p>Počasí: </p>
+        <h2>Předpověď pro město {weather ? weather.city : "načítání"}</h2>
+        <p>Teplota: {weather ? weather.forecast[0].main.temp : "načítání"}</p>
+        <p>Počasí: {weather ? weather.forecast[0].weather[0].description : "načítání"}</p>
       </div>
     </>
   );
