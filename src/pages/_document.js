032213681b844155b3a7161fb5e2c7ec54060fe9
Dalibor Janeček <dalibor.janecek@gmail.com>
2024-10-06 20:33:54 +0200

    změna responzivního layoutu

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="cs">
-      <Head />
+      <Head>
+        <title>Počasí</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
