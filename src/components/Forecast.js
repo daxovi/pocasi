3b874655b7f6495668dfff448b1eea094890cfe6
Dalibor Janeček <dalibor.janecek@gmail.com>
2024-10-07 22:36:23 +0200

    graf vývoje teploty

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ForecastItem from './ForecastItem';
 
-const Forecast = ({ forecastList, timezoneOffset, isCelsius}) => {
+const Forecast = ({ forecastList, timezoneOffset, isCelsius }) => {
     const formatDate = (unixTime, timezoneOffset) => {
         const actualTimezoneOffset = -new Date().getTimezoneOffset() * 60;
         const date = new Date((unixTime + timezoneOffset - actualTimezoneOffset) * 1000);
@@ -23,17 +23,37 @@ const Forecast = ({ forecastList, timezoneOffset, isCelsius}) => {
         ))
     }
 
+    const getMinMaxTemperature = (dayForecast) => {
+        const forecastminmax = dayForecast.map((item) => (
+            item.main.temp
+        ))
+        return [Math.min(...forecastminmax), Math.max(...forecastminmax)];
+    }
+
+    const getPercentageTemperature = (temperature, min, max) => {
+        const normalized = (temperature - min) / (max - min);
+        const percentage = normalized * 80 + 10;
+        return Math.min(Math.max(percentage, 10), 90);
+    };
+
     return (
         <div>
-            {Object.keys(forecastByDay).map((day, index) => (
+            {Object.keys(forecastByDay).map((day, indexDay) => (
                 <div key={day} className="forecast-day">
                     <h2>{day}</h2>
                     <div className="forecast-day--row">
-                        {index === 0 && emptyItems(forecastByDay[day])}
-                        {forecastByDay[day].map((forecast) => (
-                            <ForecastItem key={forecast.dt} forecast={forecast} timezoneOffset={timezoneOffset} isCelsius={isCelsius} />
+                        {indexDay === 0 && emptyItems(forecastByDay[day])}
+                        {forecastByDay[day].map((forecast, index) => (
+                            <ForecastItem
+                                key={forecast.dt}
+                                forecast={forecast}
+                                timezoneOffset={timezoneOffset}
+                                isCelsius={isCelsius}
+                                percentageTemp={getPercentageTemperature(forecast.main.temp, getMinMaxTemperature(forecastByDay[day])[0], getMinMaxTemperature(forecastByDay[day])[1])}
+                                timeout={indexDay * 1000 + index *300}
+                            />
                         ))}
-                        {index > 0 && emptyItems(forecastByDay[day])}
+                        {indexDay > 0 && emptyItems(forecastByDay[day])}
                     </div>
                 </div>
             ))}
--- src/components/ForecastItem.js
+++ src/components/ForecastItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ForecastItem = ({ forecast, timezoneOffset, isCelsius }) => {
+const ForecastItem = ({ forecast, timezoneOffset, isCelsius, percentageTemp, timeout }) => {
     const formatTime = (unixTime, timezoneOffset) => {
         const actualTimezoneOffset = -new Date().getTimezoneOffset() * 60;
         const date = new Date((unixTime + timezoneOffset - actualTimezoneOffset) * 1000);
@@ -14,13 +14,13 @@ const ForecastItem = ({ forecast, timezoneOffset, isCelsius }) => {
     }
 
     const getTempertatureCF = (celsius) => {
+        console.log((forecast.main.temp) + " " + (percentageTemp)) // DEBUG
         if (isCelsius) {
             return Math.round(celsius);
         } else {
-            return Math.round((celsius * 9/5) + 32);
+            return Math.round((celsius * 9 / 5) + 32);
         }
-        
-      };
+    };
 
     return (
         <div className='forecast-item'>
@@ -28,8 +28,10 @@ const ForecastItem = ({ forecast, timezoneOffset, isCelsius }) => {
             <time className='forecast-item--hours'>{formatHours(forecast.dt, timezoneOffset)}</time>
             <p className='forecast-item--temperature'>{getTempertatureCF(forecast.main.temp)}<span>{isCelsius ? "°C" : "°F"}</span></p>
             <img src={"/icons/" + forecast.weather[0].icon + ".svg"} alt={forecast.weather[0].description} />
+            <div className="forecast-item--graph" style={{ height: percentageTemp + '%', animationDelay: timeout + "ms"}}>
+            </div>
             <div className='forecast-item--tooltip'>
-            <p>{forecast.weather[0].description}</p>
+                <p>{forecast.weather[0].description}</p>
                 <p>pocitová teplota<br />{getTempertatureCF(forecast.main.feels_like)}{isCelsius ? "°C" : "°F"}</p>
                 <p>rychlost větru<br />{forecast.wind.speed}m/s</p>
                 <p>oblačnost<br />{forecast.clouds.all}%</p>
